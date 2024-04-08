d4b880941d5d8b38971bc5c3ad34034794cd5b2b
arielgg46 <lenin46ariel@gmail.com>
2024-04-08 01:57:11 -0500

    Updated server

--- server/channel.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-class Channel:
-    def __init__(self,admin) :
-        self.users = [admin]
-        self.channel_admin = [admin]
--- server/irc_server.py
+++ server/irc_server.py
@@ -1,8 +1,9 @@
 import socket
 import threading
+import re
 
 class Server:
-    def __init__(self, host, port):
+    def init(self, host, port):
         self.host = host
         self.port = port
         self.channels = {}
@@ -17,9 +18,10 @@ class Server:
         while True:
             client_socket, addr = self.server_socket.accept()
             print(f"Nueva conexión de {addr}")
-            threading.Thread(target=self.handle_client, args=(client_socket,)).start()
+            client = threading.Thread(target=self.handle_client, args=(client_socket,addr))
+            client.start()
 
-    def handle_client(self, client_socket):
+    def handle_client(self, client_socket,addr):
         user_name = None
         while True:
             try:
@@ -28,14 +30,19 @@ class Server:
                     break
                 try:
                     data_decoded = data.decode('utf-8')
+                    print("data decoded")
+                    print(data_decoded)
                 except UnicodeDecodeError:
                     print("Error de codificación, ignorando datos")
                     continue
 
-                parts = data_decoded.strip().split(' ')
+                parts = re.split(r'\s+',data_decoded.strip())
                 command = parts[0]
 
-                if command == "NICK":
+                if 'USER' in data_decoded:
+                    user_name=parts[1]
+                    self.users[user_name]=client_socket
+                elif command == "NICK":
                     user_name = parts[1]
                     self.users[user_name] = client_socket
                 elif command == "JOIN":
@@ -43,13 +50,15 @@ class Server:
                     if channel_name not in self.channels:
                         self.channels[channel_name] = []
                     self.channels[channel_name].append(user_name)
+                    for user in self.channels[channel_name]:
+                        self.users[user].sendall(f":{user_name}!{user_name}@{addr[0]} JOIN {channel_name}\r\n".encode('utf-8'))
                 elif command == "PRIVMSG":
                     channel_name = parts[1]
                     message = ' '.join(parts[2:])
                     if channel_name in self.channels:
                         for user in self.channels[channel_name]:
                             if user != user_name:
-                                self.users[user].sendall(f"{user_name}: {message}\n".encode('utf-8'))
+                                self.users[user].sendall(f":{user_name}!{user_name}@{addr[0]} PRIVMSG {channel_name} : {message}\r\n".encode('utf-8'))
                 elif command == "QUIT":
                     if user_name:
                         for channel in self.channels.values():
@@ -62,6 +71,6 @@ class Server:
                 print(f"Error al procesar el mensaje: {e}")
                 break
 
-if __name__ == "__main__":
+if name == "main":
     server = Server("192.168.57.100", 6667)
     server.start()
